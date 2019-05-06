7db8217f2877665bfd0e10562189a8bb0bb29862
Aidenne <36409777+Aidenne@users.noreply.github.com>
2019-05-06 13:28:18 +0300

    full chat

--- Assets/Scripts/PlayerChat.cs
+++ Assets/Scripts/PlayerChat.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class PlayerChat : MonoBehaviour
+public class PlayerChat : NetworkBehaviour
 {
     public Text chatTextPrefab;
+    ScrollRect scrollRect;
 
-    InputField enteredText;
-    GameObject content;
+    public GameObject chatScroll;
+
+    public InputField enteredText;
+    public GameObject content;
 
     public void Start()
     {
-        //if (isLocalPlayer)
-        {
-            enteredText = GameObject.FindObjectOfType<InputField>();
-            content = GameObject.FindGameObjectWithTag("Content");
-        }
 
+        scrollRect = chatScroll.GetComponent<ScrollRect>();
     }
 
     public void AddMessage()
@@ -27,37 +26,43 @@ public class PlayerChat : MonoBehaviour
         Debug.Log(message);
 
         Text textModel = Instantiate(chatTextPrefab);
-        Debug.Log(textModel == null);
-        Debug.Log(chatTextPrefab == null);
-        Debug.Log(GameObject.FindGameObjectsWithTag("Message").Length);
-        Debug.Log(content==null);
-        textModel.transform.SetParent(content.transform);
+
+       
+        textModel.transform.SetParent(content.transform,false);
+        Debug.Log(textModel.transform.position);
+        Debug.Log(textModel.transform.parent);
 
         textModel.text = $"{LocalizationManager.instance.GetLocalizedValue("You: ")} {message}";
         textModel.transform.localScale = new Vector3(1, 1, 1);
 
         enteredText.text = "";
+        scrollRect.velocity = new Vector2(0f, 1000f);
 
-        //CmdSendMessage(gameObject.GetComponent<Player>().name, message);
+        enteredText.ActivateInputField();
+        enteredText.Select();
+
+        CmdSendMessage(gameObject.GetComponent<Player>().name, message);
     }
 
-    //[Command]
-    //void CmdSendMessage(string name,string message)
-    //{
-    //    RpcSendMessage(name,message);
-    //}
+    [Command]
+    void CmdSendMessage(string name, string message)
+    {
+        RpcSendMessage(name, message);
+    }
 
-    //[ClientRpc]
-    //void RpcSendMessage(string name, string message)
-    //{
-    //    if (isLocalPlayer) return;
-    //    Text textModel = Instantiate(chatTextPrefab);
+    [ClientRpc]
+    void RpcSendMessage(string name, string message)
+    {
+        if (isLocalPlayer) return;
+        Text textModel = Instantiate(chatTextPrefab);
 
-    //    textModel.transform.SetParent(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerChat>().content.transform);
-    //    textModel.transform.localScale = new Vector3(1, 1, 1);
+        textModel.transform.SetParent(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerChat>().content.transform);
+        textModel.transform.localScale = new Vector3(1, 1, 1);
 
-    //    textModel.text = $"{name}:  {message}";
-    //}
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerChat>().scrollRect.velocity= new Vector2(0f, 1000f);
+
+        textModel.text = $"{name}:  {message}";
+    }
 
     public void ClearChat()
     {
@@ -66,6 +71,29 @@ public class PlayerChat : MonoBehaviour
         for (int i = 0; i < messages.Length; i++)
         {
             Destroy(messages[i]);
+        }
+    }
+
+    public void IncreaseImage(GameObject image)
+    {
+        image.GetComponent<RectTransform>().localScale = new Vector3(1.1f, 1.1f);
+    }
+
+    public void DecreaseImage(GameObject image)
+    {
+        image.GetComponent<RectTransform>().localScale = new Vector3(1, 1f);
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            enteredText.ActivateInputField();
+
+            if (enteredText.isFocused && enteredText.text != "")
+                AddMessage();
         }
     }
 
